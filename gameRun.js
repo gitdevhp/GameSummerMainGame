738d56607feff2762b36692e92ec63f6e653f7e1
Jaden Cai <67614448+gitdevhp@users.noreply.github.com>
2023-06-16 16:03:49 -0500

    l,

--- gameRun.js
+++ gameRun.js
@@ -1,5 +1,24 @@
 import data from './enemy.json' assert {type: 'json'};
 console.log(data);
 
-var curLvl = document.getElementById('lvl').innerHTML;
+var curLvl = 0;
 let currEnem = JSON.parse(data);
+
+function setLvlEnem() {
+    var solCount = currEnem[curLvl].SoldierNum;
+    var hostageCount = currEnem[curLvl].HostageNum;
+    for (let i = 0; i < SoldierNum; i++) {
+        spawnSoldier();     
+    }
+    for (let p = 0; p < array.length; p++) {
+        spawnHostage();        
+    }
+}
+
+function spawnSoldier(){
+    
+}
+
+function spawnHostage(){
+
+}
