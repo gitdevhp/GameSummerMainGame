3324ad47e607c71d84a90815e02efe0fbb08d72a
Jaden Cai <67614448+gitdevhp@users.noreply.github.com>
2023-06-17 14:17:17 -0500

    bbc

--- gameRun.js
+++ gameRun.js
@@ -13,11 +13,11 @@ async function main() {
 
     function setLvlEnem() {
         var solCount = currEnem[curLvl].SoldierNum;
-        var hostageCount = currEnem[curLvl].HostageNum;
-        for (let i = 0; i < SoldierNum; i++) {
+        var hosCount = currEnem[curLvl].HostageNum;
+        for (let i = 0; i < solCount; i++) {
             spawnGuy();     
         }
-        for (let p = 0; p < array.length; p++) {
+        for (let p = 0; p < hosCount; p++) {
             spawnHos();        
         }
     }
