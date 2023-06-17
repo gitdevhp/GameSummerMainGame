543feaba237e4ef35181f91452f391877281536c
Jaden Cai <67614448+gitdevhp@users.noreply.github.com>
2023-06-17 12:28:38 -0500

    is this real chat

--- gameRun.js
+++ gameRun.js
@@ -1,37 +1,49 @@
-import data from './enemy.json' assert {type: 'json'};
-console.log(data);
+//import data from './enemy.json' assert {type: 'json'};
+//console.log(data);
 
-var curLvl = 0;
-let currEnem = JSON.parse(data);
 
-function setLvlEnem() {
-    var solCount = currEnem[curLvl].SoldierNum;
-    var hostageCount = currEnem[curLvl].HostageNum;
-    for (let i = 0; i < SoldierNum; i++) {
-        spawnGuy();     
+
+async function main() {
+    const response = await fetch("enemy.json");
+    const jsonData = await response.json();
+    console.log(jsonData);
+
+    var curLvl = 0;
+    currEnem = JSON.parse(jsonData);
+
+    function setLvlEnem() {
+        var solCount = currEnem[curLvl].SoldierNum;
+        var hostageCount = currEnem[curLvl].HostageNum;
+        for (let i = 0; i < SoldierNum; i++) {
+            spawnGuy();     
+        }
+        for (let p = 0; p < array.length; p++) {
+            spawnHos();        
+        }
     }
-    for (let p = 0; p < array.length; p++) {
-        spawnHos();        
+
+    function spawnGuy() {
+        var position = Math.floor(Math.random()*3)+1;
+        console.log(position);
+        const newGuy = document.createElement('p');
+        if(position==1) {
+            newGuy.setAttribute("id", "guyLower");
+        } else if (position==2) {
+            newGuy.setAttribute("id", "guyMiddle");
+        } else {
+            newGuy.setAttribute("id", "guyTop");
+        }
+        newGuy.setAttribute('style',"display: none;")
+        newGuy.innerHTML='enemGuy';
+        document.getElementById("enemHold").appendChild(newGuy);
+        console.log("created Guy");
+    }
+
+    function spawnHos(){
+        const existGuy = document.get
     }
 }
 
-function spawnGuy() {
-    var position = Math.floor(Math.random()*3)+1;
-    console.log(position);
-    const newGuy = document.createElement('p');
-    if(position==1) {
-        newGuy.setAttribute("id", "guyLower");
-    } else if (position==2) {
-        newGuy.setAttribute("id", "guyMiddle");
-    } else {
-        newGuy.setAttribute("id", "guyTop");
-    }
-    newGuy.setAttribute('style',"display: none;")
-    newGuy.innerHTML='enemGuy';
-    document.getElementById("enemHold").appendChild(newGuy);
-    console.log("created Guy");
-}
+  main();
 
-function spawnHos(){
 
-}
